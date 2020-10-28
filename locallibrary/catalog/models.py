a3f76a1b10059454f9809b62f0c2936e42c08097
galfimbl95 <galfimbl95@mail.ru>
2020-10-28 21:23:04 +0700

    '202010282123'

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -19,6 +19,14 @@ class Genre(models.Model):
         """
         return self.name
 
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
 
 class Book(models.Model):
     """
@@ -36,6 +44,7 @@ class Book(models.Model):
         Genre, help_text="Select a genre for this book")
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """
