83a1be217243396c1640ca1361ddd0b097ebc493
galfimbl95 <galfimbl95@mail.ru>
2020-10-30 17:27:18 +0700

    '202010301727'

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book, Author, BookInstance, Genre, Language
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    num_genre=Genre.objects.count()
+    num_books_shekli=Book.objects.filter(title__icontains='Пушка').count()
+    
+    # Отрисовка HTML-шаблона index.html с данными внутри 
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={
+        'num_books':num_books,
+        'num_instances':num_instances,
+        'num_instances_available':num_instances_available,
+        'num_authors':num_authors,
+        'num_genre':num_genre,
+        'num_books_shekli':num_books_shekli,
+
+        },
+    )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
